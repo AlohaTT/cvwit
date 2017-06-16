603b32f84a85f88e86d3105027295be42ca73aab
tony <tony@tonydeiMac.local>
2017-06-16 18:46:13 +0800

    修改aticle的foreignKey

--- news/models.py
+++ news/models.py
@@ -28,7 +28,7 @@ class Column(models.Model):
 
 @python_2_unicode_compatible
 class Article(models.Model):
-    column = models.ManyToManyField(Column, verbose_name='归属栏目')
+    column = models.ForeignKey(Column, verbose_name='归属栏目')
 
     title = models.CharField('标题', max_length=256)
     slug = models.CharField('网址', max_length=256, db_index=True)
